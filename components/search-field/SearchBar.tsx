41b361524dc4791aad760cdeaa7558b3174a2933
Nelson Rosales <nelson.rosales83@gmail.com>
2022-10-08 12:51:53 +0200

    feat: search bar implemented

---
+++ components/search-field/SearchBar.tsx
@@ -0,0 +1,30 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { BiSearchAlt } from "react-icons/bi";
+
+type SearchBarProps = {
+  onSearch: string;
+  onTermChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SearchBar = ({ onSearch, onTermChange }: SearchBarProps) => {
+  return (
+    <>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="country-search">
+          <BiSearchAlt size={20} />
+        </InputGroup.Text>
+        <Form.Control
+          onChange={onTermChange}
+          value={onSearch}
+          placeholder="Search by country name"
+          aria-label="country"
+          aria-describedby="country-search"
+        />
+      </InputGroup>
+    </>
+  );
+};
+
+export default SearchBar;
--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Table from "react-bootstrap/Table";
 import Link from "next/link";
 import Container from "react-bootstrap/Container";
@@ -10,15 +11,22 @@ import { selectCart } from "../../redux/slices/cartSlice";
 import cartSlice, { addToCart } from "../../redux/slices/cartSlice";
 import { IoIosPeople } from "react-icons/io";
 import { FaRulerCombined } from "react-icons/fa";
+import SearchBar from "../search-field/SearchBar";
 
 const TableContent = () => {
   const cart = useSelector(selectCart);
   const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = React.useState("");
   const { data, error, isLoading } = useFetchAllCountriesQuery();
   console.log(data);
 
+  const handleTermInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <Container className={styles.container}>
+      <SearchBar onSearch={searchTerm} onTermChange={handleTermInput} />
       {isLoading ? <SpinnerComponent /> : null}
       <Table
         striped
@@ -39,55 +47,59 @@ const TableContent = () => {
           </tr>
         </thead>
         <tbody>
-          {data?.map((country: Country) => (
-            <tr key={country.name.common}>
-              <td>
-                <Link href={`/${country.name.common}`}>
-                  <img
-                    src={country.flags.png}
-                    alt={country.name.common}
-                    width={80}
-                    height={50}
-                    className={styles.link}
-                  />
-                </Link>
-              </td>
+          {data
+            ?.filter((country: Country) =>
+              country?.name.common.toLowerCase().includes(searchTerm)
+            )
+            .map((country: Country) => (
+              <tr key={country.name.common}>
+                <td>
+                  <Link href={`/${country.name.common}`}>
+                    <img
+                      src={country.flags.png}
+                      alt={country.name.common}
+                      width={80}
+                      height={50}
+                      className={styles.link}
+                    />
+                  </Link>
+                </td>
 
-              <td>
-                <Link href={`/${country.name.common}`} passHref>
-                  <a>{country.name.common}</a>
-                </Link>
-              </td>
+                <td>
+                  <Link href={`/${country.name.common}`} passHref>
+                    <a>{country.name.common}</a>
+                  </Link>
+                </td>
 
-              <td>{country.capital}</td>
-              <td>{country.region}</td>
+                <td>{country.capital}</td>
+                <td>{country.region}</td>
 
-              <td>
-                <ul>
-                  {/* display population as number */}
-                  <li>
-                    <IoIosPeople size={20} />:{" "}
-                    {Number(country.population).toLocaleString()}
-                  </li>
-                  <li>
-                    <FaRulerCombined size={15} />:{" "}
-                    {Number(country.area).toLocaleString()} km²
-                  </li>
-                </ul>
-              </td>
-              <td className="mx-auto">
-                <button
-                  className={styles.addBtn}
-                  onClick={() => dispatch(addToCart(country))}
-                  disabled={cart.some((item: Country) =>
-                    item.name.common === country.name.common ? true : false
-                  )}
-                >
-                  Add to Cart
-                </button>
-              </td>
-            </tr>
-          ))}
+                <td>
+                  <ul>
+                    {/* display population as number */}
+                    <li>
+                      <IoIosPeople size={20} />:{" "}
+                      {Number(country.population).toLocaleString()}
+                    </li>
+                    <li>
+                      <FaRulerCombined size={15} />:{" "}
+                      {Number(country.area).toLocaleString()} km²
+                    </li>
+                  </ul>
+                </td>
+                <td className="mx-auto">
+                  <button
+                    className={styles.addBtn}
+                    onClick={() => dispatch(addToCart(country))}
+                    disabled={cart.some((item: Country) =>
+                      item.name.common === country.name.common ? true : false
+                    )}
+                  >
+                    Add to Cart
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </Table>
     </Container>
