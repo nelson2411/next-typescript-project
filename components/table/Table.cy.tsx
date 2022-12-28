9909a0d02bb29bcb389176f6a9b2786c105882c3
Nelson Rosales <nelson.rosales83@gmail.com>
2022-12-28 20:35:47 +0100

    fix: table test

--- components/table/Table.cy.tsx
+++ components/table/Table.cy.tsx
@@ -1,9 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { render } from '@testing-library/react';
 import Table from './Table';
 
-describe('It should mount the table', () => {
-  it('should mount the component', () => {
-    cy.mount(<Table />);
+describe('TableContent', () => {
+  it('renders the table with the correct content and functionality', () => {
+    cy.visit('/');
+    cy.get('table').should('exist');
+    cy.get('table tbody tr').should('have.length', 3); // 3 rows in the table
+    cy.get('table tbody tr:first-child td:first-child').should('contain', 'Country A');
+    cy.get('table tbody tr:last-child td:first-child').should('contain', 'Country C');
+    cy.get('table tbody tr:first-child td:last-child button').click();
+    cy.get('table tbody tr:first-child td:last-child button').should('contain', 'Remove from cart');
   });
 });
-
-// Table component must have a data-cy attribute
